c5ed8ff41cb68ce43ec5e46b8a5eb27996b9c9da
Anastasiya145 <asiva@ukr.net>
2024-08-21 12:28:05 +0300

    fix pg module was installed manually

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,23 +1,19 @@
-'use strict';
+"use strict";
 
-require('dotenv/config');
+require("dotenv/config");
+const pg = require("pg");
 
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
-const {
-  DB_HOST,
-  DB_NAME,
-  DB_USERNAME,
-  DB_DIALECT,
-  DB_PASSWORD,
-  DB_PORT,
-} = process.env;
+const { DB_HOST, DB_NAME, DB_USERNAME, DB_DIALECT, DB_PASSWORD, DB_PORT } =
+  process.env;
 
 const client = new Sequelize({
   host: DB_HOST,
   database: DB_NAME,
   username: DB_USERNAME,
   dialect: DB_DIALECT,
+  dialectModule: pg,
   password: DB_PASSWORD,
   port: DB_PORT,
   dialectOptions: {
